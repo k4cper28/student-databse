dccb315048ecc0404f1740d9f34b5bd6049a88ad
k4cper28 <kacpersciuba@gmail.com>
2023-08-15 02:07:24 +0200

    add sort by lastname lower and higher

--- src/test.cpp
+++ src/test.cpp
@@ -3,107 +3,9 @@
 #include "student.hpp"
 #include <string>
 
-struct DatabaseTest : ::testing::Test{
+struct DatabaseTest : ::testing::Test {
     Database db;
-};
 
-
-TEST_F(DatabaseTest, DisplayEmptyDb){
-    auto content = db.show();
-    auto expected ="";
-    EXPECT_EQ(content, expected);
-}
-
-TEST_F(DatabaseTest, DisplayNonEmptyDb){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    db.add(adam);
-
-    auto content = db.show();
-    auto expected ="Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
-    EXPECT_EQ(content, expected);
-
-}
-
-TEST_F(DatabaseTest, NoFindByLastname){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    db.add(adam);
-
-    auto content = db.findByLastname("Nowak");
-    std::string expected = "";
-    EXPECT_EQ(content, expected);
-}
-
-
-TEST_F(DatabaseTest, FindByLastname){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-
-    db.add(adam);
-
-    auto content = db.findByLastname("Kowalski");
-    std::string expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
-    EXPECT_EQ(content, expected);
-}
-
-TEST_F(DatabaseTest, NoFindByPesel){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    db.add(adam);
-
-    auto content = db.findByPesel("11223344569");
-    std::string expected = "";
-    EXPECT_EQ(content, expected);
-}
-
-TEST_F(DatabaseTest, FindByPesel){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    db.add(adam);
-
-    auto content = db.findByPesel("11223344567");
-    std::string expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
-    EXPECT_EQ(content, expected);
-}
-
-
-TEST_F(DatabaseTest, SortByPeselHigh){
     Student adam{
             "Adam",
             "Kowalski",
@@ -130,7 +32,66 @@ TEST_F(DatabaseTest, SortByPeselHigh){
             "96544332211",
             Gender::Female
     };
+};
 
+
+TEST_F(DatabaseTest, DisplayEmptyDb){
+    auto content = db.show();
+
+    auto expected ="";
+
+    EXPECT_EQ(content, expected);
+}
+
+TEST_F(DatabaseTest, DisplayNonEmptyDb){
+    db.add(adam);
+
+    auto content = db.show();
+    auto expected ="Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
+
+    EXPECT_EQ(content, expected);
+
+}
+
+TEST_F(DatabaseTest, NoFindByLastname){
+    db.add(adam);
+
+    auto content = db.findByLastname("Nowak");
+    std::string expected = "";
+
+    EXPECT_EQ(content, expected);
+}
+
+
+TEST_F(DatabaseTest, FindByLastname){
+    db.add(adam);
+
+    auto content = db.findByLastname("Kowalski");
+    std::string expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
+
+    EXPECT_EQ(content, expected);
+}
+
+TEST_F(DatabaseTest, NoFindByPesel){
+    db.add(adam);
+
+    auto content = db.findByPesel("11223344569");
+    std::string expected = "";
+
+    EXPECT_EQ(content, expected);
+}
+
+TEST_F(DatabaseTest, FindByPesel){
+    db.add(adam);
+
+    auto content = db.findByPesel("11223344567");
+    std::string expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
+
+    EXPECT_EQ(content, expected);
+}
+
+
+TEST_F(DatabaseTest, SortByPeselHigh){
     db.add(adam);
     db.add(karol);
     db.add(klaudia);
@@ -139,37 +100,11 @@ TEST_F(DatabaseTest, SortByPeselHigh){
     std::string expected = "Klaudia Boobek; ul. Dobra 134, 00-200 Warszawa; 123458; 96544332211; Female; "
                            "Karol Nowak; ul. Dobra 134, 00-200 Warszawa; 123457; 76544332211; Male; "
                            "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; ";
+
     EXPECT_EQ(content, expected);
 }
 
 TEST_F(DatabaseTest, SortByPeselLow){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    Student karol{
-            "Karol",
-            "Nowak",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123457,
-            "76544332211",
-            Gender::Male
-    };
-
-    Student klaudia{
-            "Klaudia",
-            "Boobek",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123458,
-            "96544332211",
-            Gender::Female
-    };
-
     db.add(klaudia);
     db.add(karol);
     db.add(adam);
@@ -178,37 +113,11 @@ TEST_F(DatabaseTest, SortByPeselLow){
     std::string expected = "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; "
                            "Karol Nowak; ul. Dobra 134, 00-200 Warszawa; 123457; 76544332211; Male; "
                            "Klaudia Boobek; ul. Dobra 134, 00-200 Warszawa; 123458; 96544332211; Female; ";
+
     EXPECT_EQ(content, expected);
 }
 
 TEST_F(DatabaseTest, SortByLastnameHigh){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    Student karol{
-            "Karol",
-            "Nowak",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123457,
-            "76544332211",
-            Gender::Male
-    };
-
-    Student klaudia{
-            "Klaudia",
-            "Boobek",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123458,
-            "96544332211",
-            Gender::Female
-    };
-
     db.add(adam);
     db.add(karol);
     db.add(klaudia);
@@ -217,38 +126,12 @@ TEST_F(DatabaseTest, SortByLastnameHigh){
     std::string expected = "Karol Nowak; ul. Dobra 134, 00-200 Warszawa; 123457; 76544332211; Male; "
                            "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; "
                            "Klaudia Boobek; ul. Dobra 134, 00-200 Warszawa; 123458; 96544332211; Female; ";
+
     EXPECT_EQ(content, expected);
 }
 
 
 TEST_F(DatabaseTest, SortByLastnameLow){
-    Student adam{
-            "Adam",
-            "Kowalski",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123456,
-            "11223344567",
-            Gender::Male
-    };
-
-    Student karol{
-            "Karol",
-            "Nowak",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123457,
-            "76544332211",
-            Gender::Male
-    };
-
-    Student klaudia{
-            "Klaudia",
-            "Boobek",
-            "ul. Dobra 134, 00-200 Warszawa",
-            123458,
-            "96544332211",
-            Gender::Female
-    };
-
     db.add(klaudia);
     db.add(karol);
     db.add(adam);
@@ -257,5 +140,6 @@ TEST_F(DatabaseTest, SortByLastnameLow){
     std::string expected =  "Klaudia Boobek; ul. Dobra 134, 00-200 Warszawa; 123458; 96544332211; Female; "
                             "Adam Kowalski; ul. Dobra 134, 00-200 Warszawa; 123456; 11223344567; Male; "
                             "Karol Nowak; ul. Dobra 134, 00-200 Warszawa; 123457; 76544332211; Male; ";
+
     EXPECT_EQ(content, expected);
 }
